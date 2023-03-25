b02dc093de0d037b0ebf8115f5f95e47f4bb63d5
ali <aligodt@gmail.com>
2023-03-25 03:54:45 +0100

    goofy

--- main.fsx
+++ main.fsx
@@ -1,21 +1,21 @@
-
-//#r "FSharp.Json"
+#r "nuget: Newtonsoft.Json, 13.0.3"
 open System.Diagnostics
 open FSharp.Data
+open Newtonsoft.Json
 
-type RecordType = {
-    area: string
-    forecast: array[]
-}
-type RecordType2 = {
+
+
+type ForecastData = {
     time: string
     temperature: float
     humidity: float
     wind: float
     pressure: float
 }
-
-
+type Forecast = {
+    area: string
+    forecast: ForecastData[]  // important detail here is that we want a list of ForecastData
+}
 
 let apiUrls = [| "https://incommodities.io/a?area=Eagle%20River"
                  "https://incommodities.io/a?area=Kincaid%20Park"
@@ -33,44 +33,43 @@ let main () =
     for apiUrl in apiUrls do        
         let stopWatch = System.Diagnostics.Stopwatch.StartNew()
 
+
         let psi = new ProcessStartInfo("curl", sprintf "\"%s\" -X POST -H \"Authorization: Bearer %s\"" apiUrl authToken)
         psi.RedirectStandardOutput <- true
-        let process = Process.Start(psi)
-        let response = process.StandardOutput.ReadToEnd()
+        let proces = Process.Start(psi)
+        let response = proces.StandardOutput.ReadToEnd()
         printfn "Response %d: %s" (x+1) response
         x <- x + 1
-        thisString = response
-        if not (lastString=thisString) then 
-            printfn "CHANGED!"
-        printfn "%s" lastString
-        printfn "%s" thisString
-        lastString = response
         
-    
-        newLineSplit = response.Split "\n"
-        dataSplit = newLineSplit.Split ","
-        time = dataSplit[0]
-        temperature = dataSplit[2]
-        humidity = dataSplit[3]
-        wind = dataSplit[4]
-        pressure = dataSplit[5]
-        //let data: RecordType2 = {time = time, temperature = temperature, humidity = humidity, wind = wind , pressure = pressure}
 
-        let deserialized = Json.deserialize<RecordType> json
+        // From here we disect the response from the API.
+        try
+            let newLineSplit = Seq.toList(response.Split "\n")
+            let mutable forecasts = Array.empty
+            for i in 1.. newLineSplit.Length-1 do
+                let dataSplit = newLineSplit.[i].Split ","
 
-        let data2: RecordType = { area = "AREA"; forecast = [|1;2;3|] }
-        let json = Json.serialize data2
-        printfn "%s" json
-        // json is """{ "stringMember": "The string", "intMember": 123 }"""
+                let time = dataSplit.[0]
+                let temperature = dataSplit.[2]
+                let humidity = dataSplit.[3]
+                let wind = dataSplit.[4]
+                let pressure = dataSplit.[5]
+                let somecastData = {time = time ; temperature =  float temperature ; humidity = float humidity; wind = float wind; pressure = float pressure} 
+                forecasts <- Array.append forecasts [| somecastData |]
+            
+            let forecast = { area = "AREA"; 
+            forecast = forecasts}
 
-        // deserialize from JSON to record
-        let deserialized = Json.deserialize<RecordType> json
-        printfn "%A" deserialized
-
-        stopWatch.Stop()
-        printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+            let json = JsonConvert.SerializeObject(forecast)
+            System.IO.File.WriteAllText($"forecast{x}.json", json)
+            
+            stopWatch.Stop()
+            printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+        with
+            | ex -> printfn "An error occurred: %s\nOriginal response: %s" ex.Message response
 
     printfn "API call completed %d times" x
+
 
 
 main()
